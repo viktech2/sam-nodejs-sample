3926eb7c470f68c24c1b699535c3070c9b776269
Vikash S <vikash.shaw@imerit.net>
2021-01-19 15:34:13 +0530

    event and debug launch conf added

--- commands.sh
+++ commands.sh
@@ -9,3 +9,6 @@ aws cloudformation package --s3-bucket vik-code-sam --template-file template.yam
 # deploy
 aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
 # sam deploy ...
+
+# local invoke
+sam local invoke -e event.json --debug-port 5858 helloworld
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ console.log('Loading function');
 
 exports.handler = async (event, context) => {
     
+    console.log('DEBUG: event name is ', event.name);
     return {
         'statusCode': 200,
         'body': JSON.stringify({
