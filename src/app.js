b0ef256517e6c2ece2b376a9a1f73c88a868de4f
Vikash S <vikash.shaw@imerit.net>
2021-01-19 23:26:17 +0530

    API Gateway added

--- src/app.js
+++ src/app.js
@@ -4,12 +4,26 @@ console.log('Loading function');
 
 exports.handler = async (event, context) => {
     
-    console.log('DEBUG: event name is ', event.name);
+    /* console.log('DEBUG: event name is ', event.name);
     return {
         'statusCode': 200,
         'body': JSON.stringify({
             message: 'hello world!!!'
         })
+    }; */
+
+    const done = (err, res) => {
+
+        return {
+            statusCode: err ? '400' : '200',
+            body: err ? err.message : JSON.stringify(res),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
     };
-    
+
+    return done(null, 'hello world!!!');
+
+
 };
